f271b336f32a3fc0df8349017d155b1cbc43627a
Ben Rochberg <benrochberg@gmail.com>
2022-11-09 13:18:31 -0500

    Main Menu UI Initial Commit
    - An Art folder with UI and Font sub folders have been created.
    - 3 Buttons (PlayButton, OtherButton, and QandBButton) have been imported to the UI folder.
    - 3 Fonts (BebasNeue, Japanese_2020 Italic, and Japanese_2020 SDF) have been imported to the Fonts folder.
    - Created a new color palette Library. Stored in the Editor folder.
    - Implemented the 3rd Menu Layout option I posted in Discord.
    - Added Animations to clicking the OptBtn, HelpBtn, and CredBtn which will open up their respective sub menus and close the Main Menu.
    - Pressing the "ESC" key when a sub menu is open will play an animation closing the sub menu and opening the Main Menu.

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,32 +1,82 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
 
-    public GameObject button;
+    public GameObject credBtn, credMenu, mainMenu, helpMenu, optMenu;
+    public TextMeshProUGUI topText;
 
     bool IsBig;
 
 
     void Start()
     {
+        topText.text = "HIDDEN  KUNAI";
         IsBig = false;
     }
-  
-    public void ButtonBig()
+
+    public void OptBig()
     {
         IsBig = true;
-        LeanTween.scale(button, new Vector2(2f,2f),1f).setEase(LeanTweenType.easeOutBounce);
-        LeanTween.moveLocal(button, new Vector2(0f, 0f),1f).setEase(LeanTweenType.easeOutBounce);
+        topText.text = "OPTIONS";
+        credMenu.SetActive(false);
+        helpMenu.SetActive(false);
+        LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.alpha(optMenu.GetComponent<RectTransform>(), 1f, 0.15f);
+        LeanTween.scale(optMenu, new Vector2(3.09f,2.07f),0.3f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.moveLocal(optMenu, new Vector2(0f, -90f),0.3f).setEase(LeanTweenType.easeOutQuint);
+    }
+
+    public void HelpBig()
+    {
+        IsBig = true;
+        topText.text = "HELP";
+        credMenu.SetActive(false);
+        optMenu.SetActive(false);
+        LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.alpha(helpMenu.GetComponent<RectTransform>(), 1f, 0.15f);
+        LeanTween.scale(helpMenu, new Vector2(3.09f,2.07f),0.3f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.moveLocal(helpMenu, new Vector2(0f, -90f),0.3f).setEase(LeanTweenType.easeOutQuint);
+    }
+  
+    public void CredBig()
+    {
+        IsBig = true;
+        topText.text = "CREDITS";
+        optMenu.SetActive(false);
+        helpMenu.SetActive(false);
+        LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.alpha(credMenu.GetComponent<RectTransform>(), 1f, 0.15f);
+        LeanTween.scale(credMenu, new Vector2(3.09f,2.07f),0.3f).setEase(LeanTweenType.easeOutQuint);
+        LeanTween.moveLocal(credMenu, new Vector2(0f, -90f),0.3f).setEase(LeanTweenType.easeOutQuint);
     }
 
     void Update()
     {
         if (Input.GetKeyDown("escape") && IsBig == true)
         {
-            LeanTween.scale(button, new Vector2(1f,1f),1f).setEase(LeanTweenType.easeOutBounce);
-            LeanTween.moveLocal(button, new Vector2(450f, -200f),1f).setEase(LeanTweenType.easeOutBounce);
-            
+            IsBig = false;
+            topText.text = "HIDDEN  KUNAI";
+            optMenu.SetActive(true);
+            helpMenu.SetActive(true);
+            credMenu.SetActive(true);
+
+            LeanTween.alpha(optMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
+            LeanTween.scale(optMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.moveLocal(optMenu, new Vector2(-507f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+
+            LeanTween.alpha(helpMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
+            LeanTween.scale(helpMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.moveLocal(helpMenu, new Vector2(0f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            //LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+
+            LeanTween.alpha(credMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
+            LeanTween.scale(credMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.moveLocal(credMenu, new Vector2(507f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            //LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
         }
     }
 
