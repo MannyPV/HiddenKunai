daed15b012c8a6d1ef3eeeef2fc577a1f57fd3e2
Ben Rochberg <benrochberg@gmail.com>
2022-11-09 23:23:37 -0500

    Testing some UI animations and colors
    - Adjusted the PlayTran code.
    - Added hex code strings to change text or gameobject colors to specific hexacodes.
    - Pressing the Play Button will change the title text from white to black.

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -6,8 +6,12 @@ using UnityEngine.SceneManagement;
 public class MainMenuManager : MonoBehaviour
 {
 
+    [SerializeField] string whiteHex;
+    [SerializeField] string blackHex;
+
     public GameObject credBtn, credMenu, mainMenu, helpMenu, optMenu, playTran;
     public TextMeshProUGUI topText;
+
 
     bool IsBig;
 
@@ -21,9 +25,11 @@ public class MainMenuManager : MonoBehaviour
 
     public void PlayClicked()
     {
+        IsBig = true;
         optMenu.SetActive(false);
         helpMenu.SetActive(false);
         credMenu.SetActive(false);
+        topText.text = ("<color=" + blackHex + ">HIDDEN  KUNAI</color>");
         LeanTween.alpha(playTran.GetComponent<RectTransform>(), 1f, 0.5f);
         LeanTween.moveLocal(playTran, new Vector2(0f, 0f),0.2f).setEase(LeanTweenType.easeOutQuint);
         LeanTween.scale(playTran, new Vector2(1.3f,2.73f),0.5f).setEase(LeanTweenType.easeOutQuint);
@@ -35,6 +41,7 @@ public class MainMenuManager : MonoBehaviour
         IsBig = true;
         topText.text = "OPTIONS";
         topText.fontSize = 110;
+        playTran.SetActive(false);
         credMenu.SetActive(false);
         helpMenu.SetActive(false);
         LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
@@ -48,6 +55,7 @@ public class MainMenuManager : MonoBehaviour
         IsBig = true;
         topText.text = "HELP";
         topText.fontSize = 110;
+        playTran.SetActive(false);
         credMenu.SetActive(false);
         optMenu.SetActive(false);
         LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
@@ -59,8 +67,9 @@ public class MainMenuManager : MonoBehaviour
     public void CredBig()
     {
         IsBig = true;
-        topText.text = "CREDITS";
+        topText.text = "Credits";
         topText.fontSize = 110;
+        playTran.SetActive(false);
         optMenu.SetActive(false);
         helpMenu.SetActive(false);
         LeanTween.scale(mainMenu, new Vector2(0f,0f),0.1f).setEase(LeanTweenType.easeOutQuint);
@@ -76,24 +85,29 @@ public class MainMenuManager : MonoBehaviour
             IsBig = false;
             topText.text = "HIDDEN  KUNAI";
             topText.fontSize = 100;
+            
+            playTran.SetActive(true);
             optMenu.SetActive(true);
             helpMenu.SetActive(true);
             credMenu.SetActive(true);
 
+            LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
+
+            LeanTween.alpha(playTran.GetComponent<RectTransform>(), 0.35f, 0.5f);
+            LeanTween.moveLocal(playTran, new Vector2(0f, 120f),0.3f).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.scale(playTran, new Vector2(1f,1f),0.5f).setEase(LeanTweenType.easeOutQuint);
+
             LeanTween.alpha(optMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
             LeanTween.scale(optMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
             LeanTween.moveLocal(optMenu, new Vector2(-507f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
-            LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
 
             LeanTween.alpha(helpMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
             LeanTween.scale(helpMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
             LeanTween.moveLocal(helpMenu, new Vector2(0f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
-            //LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
 
             LeanTween.alpha(credMenu.GetComponent<RectTransform>(), 0.35f, 0.3f);
             LeanTween.scale(credMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
             LeanTween.moveLocal(credMenu, new Vector2(507f, -300f),0.3f).setEase(LeanTweenType.easeOutQuint);
-            //LeanTween.scale(mainMenu, new Vector2(1f,1f),0.3f).setEase(LeanTweenType.easeOutQuint);
         }
     }
 
