18695a305a2b313182d00a14acd1744676bbbb5d
Ben Rochberg <benrochberg@gmail.com>
2022-11-17 00:00:05 -0500

    Medals
    - Added 3 stars as medals.
    - When the player reaches 500 points the bronze star will appear. It will stay active until the player reaches 1000 points.
    - When the player reaches 1000 points the silver star will appear. It will stay active until the players reaches 1500 points.
    - When the player reaches 1500 points the gold star will appear.

--- Assets/Scripts/UI/ScoreManager.cs
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -4,17 +4,45 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public GameObject scoreBtn;
+
+    [SerializeField] string bronzeHex;
+    [SerializeField] string silverHex;
+    [SerializeField] string goldHex;
+
+    public GameObject scoreBtn, bMedal, sMedal, gMedal;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI addText;
 
     public int score;
     public int addScore;
 
+    void Start()
+    {
+        bMedal.SetActive(false);
+        sMedal.SetActive(false);
+        gMedal.SetActive(false);
+    } 
+
     void Update()
     {
         scoreText.text = "" + score;
         addText.text = "Add " + addScore;
+
+        if (score == 500)
+        {
+            bMedal.SetActive(true);
+        }
+        else if (score == 1000)
+        {
+            bMedal.SetActive(false);
+            sMedal.SetActive(true);
+        }
+        else if (score == 1500)
+        {
+            sMedal.SetActive(false);
+            gMedal.SetActive(true);
+        }
+
     }
 
     public void AddToScore() => score += addScore;
